eef1d3b29e7c3d4a96af0695b458023ef36fdd8d
itsKene <94003109+itsKene@users.noreply.github.com>
2023-05-03 19:52:30 +0000

    changed api endpoint again

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://lwx47zdd53.execute-api.eu-west-1.amazonaws.com/default/lambda-gabe-predict",
+                url: "https://4a28pt82sl.execute-api.eu-west-1.amazonaws.com/default/lambda-gabe-predict",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
