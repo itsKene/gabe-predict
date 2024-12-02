8d6fff93a7145c27fa25f1ce53fbebf79dd59be6
itsKene <94003109+itsKene@users.noreply.github.com>
2024-12-02 12:26:29 +0100

    Update contact_me.js
    Api gateway update

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://5qtmqkyufj.execute-api.eu-west-1.amazonaws.com/default/predict-function",
+                url: "https://w3ps53l7ik.execute-api.eu-west-1.amazonaws.com/default/predict-function",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
